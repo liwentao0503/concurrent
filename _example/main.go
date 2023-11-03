195995e50bcde0f8db8167ca84af539c66588c2b
wentao.li <wentao.li@trip.com>
2023-11-03 16:55:29 +0800

    feat: add example

---
+++ _example/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"concurrent"
+	"context"
+	"fmt"
+)
+
+func main() {
+	len := 10
+	handle := make([]concurrent.ContextHandle, 0, len)
+	resuit := make([]string, len)
+
+	var funcTemp = func(ctx context.Context, i int) error {
+		resuit[i] = fmt.Sprintf("value: %v", i)
+		if i%3 == 0 {
+			return fmt.Errorf("test err: %v", i)
+		}
+		return nil
+	}
+
+	for i := 0; i < len; i++ {
+		handle = append(handle, funcTemp)
+	}
+
+	errList := concurrent.GoAndWait(context.Background(), handle)
+	fmt.Println(resuit)
+	fmt.Println(errList)
+}
